c92efb26c152e4ea536433e61f68196bd6928d42
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2022-07-25 16:41:04 -0300

    Primeiro commit

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+	}
+}
